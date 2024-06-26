2ed48440538e3aea2092759936d0f5d909bff924
nitives <cadenrichards15@gmail.com>
2024-06-26 12:53:03 -0400

    Vercel SEO 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer, Navbar, SearchProvider, ThemeProvider } from "@/components";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -82,6 +84,8 @@ export default function RootLayout({
           </SearchProvider>
         </ThemeProvider>
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }

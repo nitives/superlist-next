ab0f57c19b8294b49737d069480981c3cdc38f60
nitives <cadenrichards15@gmail.com>
2024-06-26 12:25:46 -0400

    ReactPlayer 4

--- src/components/movie/index.ts
+++ src/components/movie/index.ts
@@ -1,6 +1,6 @@
 export * from "./MovieClient";
 export * from "./VideoPlayer";
-export * from "./XVideoPlayer";
+// export * from "./XVideoPlayer";
 export * from "./M3U8VideoPlayer";
 export * from "./MediaDetails";
 export * from "./MediaRecommendations";

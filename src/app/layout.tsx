4a7500d0d94ed59a3bbfcb9585a8b090bd9c740f
nitives <cadenrichards15@gmail.com>
2024-06-26 12:46:14 -0400

    Revert Vercel SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer, Navbar, SearchProvider, ThemeProvider } from "@/components";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -84,8 +82,6 @@ export default function RootLayout({
           </SearchProvider>
         </ThemeProvider>
       </body>
-      <Analytics />
-      <SpeedInsights />
     </html>
   );
 }

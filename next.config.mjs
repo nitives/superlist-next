0af2bf6f8628e4833072a75c45a9d7ee9c848e30
nitives <cadenrichards15@gmail.com>
2024-10-01 15:06:57 -0400

    Million Lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
+import MillionLint from '@million/lint';
 import { execSync } from "child_process";
-
 function getCommitHash() {
   return execSync("git rev-parse --short HEAD").toString().trim();
 }
@@ -8,23 +8,21 @@ function getCommitHash() {
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
-    missingSuspenseWithCSRBailout: false,
+    missingSuspenseWithCSRBailout: false
   },
   env: {
-    NEXT_PUBLIC_COMMIT_HASH: getCommitHash(),
+    NEXT_PUBLIC_COMMIT_HASH: getCommitHash()
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.flixhq.to",
-      },
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "img.flixhq.to"
+    }, {
+      protocol: "https",
+      hostname: "image.tmdb.org"
+    }]
+  }
 };
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
